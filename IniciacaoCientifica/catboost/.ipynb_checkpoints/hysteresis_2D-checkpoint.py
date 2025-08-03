7a961b09353270ff207371f36c6dd2a616e340c8
Bianca <barretoleme05@gmail.com>
2025-08-03 18:11:29 -0300

    add: correct path

--- IniciacaoCientifica/catboost/.ipynb_checkpoints/hysteresis_2D-checkpoint.py
+++ IniciacaoCientifica/catboost/.ipynb_checkpoints/hysteresis_2D-checkpoint.py
@@ -8,43 +8,43 @@ from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_percenta
 
 train_data_2D = pd.DataFrame()
 
-train_data_2D['hysteresis'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/hysteresis_all_scaled_train.csv')['total']
-train_data_2D['id'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/idiq_all_scaled_train.csv')['id']
-train_data_2D['iq'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/idiq_all_scaled_train.csv')['iq']
-train_data_2D['joule'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/joule_all_scaled_train.csv')['total']
-train_data_2D['speed'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/speed_all_scaled_train.csv')['N']
-train_data_2D['d1'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d1']
-train_data_2D['d2'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d2']
-train_data_2D['d3'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d3']
-train_data_2D['d4'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d4']
-train_data_2D['d5'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d5']
-train_data_2D['d6'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d6']
-train_data_2D['d7'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d7']
-train_data_2D['d8'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d8']
-train_data_2D['d9'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d9']
-train_data_2D['r1'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['r1']
-train_data_2D['t1'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['t1']
+train_data_2D['hysteresis'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/hysteresis_all_scaled_train.csv')['total']
+train_data_2D['id'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/idiq_all_scaled_train.csv')['id']
+train_data_2D['iq'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/idiq_all_scaled_train.csv')['iq']
+train_data_2D['joule'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/joule_all_scaled_train.csv')['total']
+train_data_2D['speed'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/speed_all_scaled_train.csv')['N']
+train_data_2D['d1'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d1']
+train_data_2D['d2'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d2']
+train_data_2D['d3'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d3']
+train_data_2D['d4'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d4']
+train_data_2D['d5'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d5']
+train_data_2D['d6'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d6']
+train_data_2D['d7'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d7']
+train_data_2D['d8'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d8']
+train_data_2D['d9'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['d9']
+train_data_2D['r1'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['r1']
+train_data_2D['t1'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_train.csv')['t1']
 
 
 
 test_data_2D = pd.DataFrame()
 
-test_data_2D['hysteresis'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/hysteresis_all_scaled_test.csv')['total']
-test_data_2D['id'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/idiq_all_scaled_test.csv')['id']
-test_data_2D['iq'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/idiq_all_scaled_test.csv')['iq']
-test_data_2D['joule'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/joule_all_scaled_test.csv')['total']
-test_data_2D['speed'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/speed_all_scaled_test.csv')['N']
-test_data_2D['d1'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d1']
-test_data_2D['d2'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d2']
-test_data_2D['d3'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d3']
-test_data_2D['d4'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d4']
-test_data_2D['d5'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d5']
-test_data_2D['d6'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d6']
-test_data_2D['d7'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d7']
-test_data_2D['d8'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d8']
-test_data_2D['d9'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d9']
-test_data_2D['r1'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['r1']
-test_data_2D['t1'] = pd.read_csv('/mnt/c/prog/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['t1']
+test_data_2D['hysteresis'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/hysteresis_all_scaled_test.csv')['total']
+test_data_2D['id'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/idiq_all_scaled_test.csv')['id']
+test_data_2D['iq'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/idiq_all_scaled_test.csv')['iq']
+test_data_2D['joule'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/joule_all_scaled_test.csv')['total']
+test_data_2D['speed'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/speed_all_scaled_test.csv')['N']
+test_data_2D['d1'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d1']
+test_data_2D['d2'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d2']
+test_data_2D['d3'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d3']
+test_data_2D['d4'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d4']
+test_data_2D['d5'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d5']
+test_data_2D['d6'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d6']
+test_data_2D['d7'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d7']
+test_data_2D['d8'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d8']
+test_data_2D['d9'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['d9']
+test_data_2D['r1'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['r1']
+test_data_2D['t1'] = pd.read_csv('~/ic_motores/IniciacaoCientifica/dataset/2D/xgeom_all_scaled_test.csv')['t1']
 
 
 
@@ -82,7 +82,7 @@ results.score.catboost = r2_score(y_test, predictions)
 results.mse.catboost = mean_squared_error(y_test, predictions)
 results.mape.catboost = mean_absolute_percentage_error(y_test, predictions)
 
-results.to_csv("/mnt/c/prog/IniciacaoCientifica/results/2D/hysteresis/results_catboost.csv")
+results.to_csv("~/ic_motores/IniciacaoCientifica/results/2D/hysteresis/results_catboost.csv")
 
 
 
@@ -91,7 +91,7 @@ data = pd.DataFrame(columns = newcolumns2)
 data.y_test = y_test
 data.y_pred = predictions
 
-data.to_csv("/mnt/c/prog/IniciacaoCientifica/pred/2D/hysteresis/pred_catboost.csv")
+data.to_csv("~/ic_motores/IniciacaoCientifica/pred/2D/hysteresis/pred_catboost.csv")
 
 
 
