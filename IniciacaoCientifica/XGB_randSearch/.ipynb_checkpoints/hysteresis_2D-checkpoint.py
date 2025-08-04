e16b4ae5a1de205dfcefd88da38311a924befa20
Bianca <barretoleme05@gmail.com>
2025-08-03 21:44:35 -0300

    change: hyperp. tuning

--- IniciacaoCientifica/XGB_randSearch/.ipynb_checkpoints/hysteresis_2D-checkpoint.py
+++ IniciacaoCientifica/XGB_randSearch/.ipynb_checkpoints/hysteresis_2D-checkpoint.py
@@ -78,7 +78,7 @@ y_test = test_data[variable]
 
 
 parameters = {
-    'n_estimators' : [100, 200],
+    'n_estimators' : [10, 20],
     'learning_rate' : [0.0001, 0.001, 0.01, 0.1, 1],
     'max_depth' : range(3, 21, 3),
     'reg_alpha' : [0, 0.5, 1, 5],
@@ -88,7 +88,7 @@ parameters = {
 rand_search = RandomizedSearchCV(estimator = XGBRegressor(random_state = 0), 
                                  param_distributions = parameters,
                                  cv = 5, 
-                                 n_iter = 20, 
+                                 n_iter = 10, 
                                  random_state = 0, 
                                  n_jobs = -1,
                                  verbose = 0)
