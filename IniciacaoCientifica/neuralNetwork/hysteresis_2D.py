2b9260409c7b1c494e07ec8bb5a3a385f60955fc
Bianca <barretoleme05@gmail.com>
2025-10-29 19:57:17 -0300

    change: 2D hysteresis pred for neural network

--- IniciacaoCientifica/neuralNetwork/hysteresis_2D.py
+++ IniciacaoCientifica/neuralNetwork/hysteresis_2D.py
@@ -85,10 +85,10 @@ def register_csv(contents, info):
 def register_txt(contents, info):
     new_row = pd.DataFrame([contents], columns = info.columns)
     
-    with open('./data/motor_2D_log.txt'):
+    with open('./data/motor_2D_log.txt') as file:
         file.write("\n")
         
-        file.write(f"Test ID: {new_now.neurons}-{new_row.layers}-{new_row.learn_rate}-{new_row.epochs}\n")
+        file.write(f"Test ID: {new_row.neurons}-{new_row.layers}-{new_row.learn_rate}-{new_row.epochs}\n")
         file.write(f"Test run at {new_row.time}\n")
     
         file.write("\n")
